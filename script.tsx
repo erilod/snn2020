b0a5a136b4bb830e2e233e84ae4c61b5ad0f1187
Erik <erik@lodetti.se>
2020-08-24 21:23:50 +0200

    Lokal cache samt kontroll om något uppdaterats på servern

--- script.tsx
+++ script.tsx
@@ -3,56 +3,91 @@ import * as Mithril from "mithril"
 
 // Fyller på med artiklar här
 let artiklar = []
+
+//För att inte fler laddningar ska göras när man fortsätter scrolla medan nya samtidigt läses in så flaggar vi om det. Den återställs till false när sidan är omritadoch nya kan läasas in vid scroll.
 let loadblock = false
-//Flagga för första gången vi laddar sidan. Så att vi alltid laddar nya artiklar efter att cachen är laddad.
-let firstcheck = true
+
+//För att skilja om laddning sker inkrementellt och arrayen ska fyllas på eller om det är en omladdning och man behöver kolla om man ska läsa in mer från servern
+enum Laddtyp {
+    reload,
+    incremental,
+}
 
 
 // När man vill ladda flera artiklar. Utgår fån längden och laddar
-function laddaArtiklar() {
-    //kollar första gången i localstorage och laddar artiklar
-    let paginate = artiklar.length
-    if (firstcheck && localStorage.getItem("artiklar")) {
-        artiklar = JSON.parse(localStorage.getItem("artiklar"))
-    }
-    
-    let url = "https://www.sydnarkenytt.se/json/etta/P" + (firstcheck ? 0 : paginate)
-    m.request({
-        url: url
-    }).then(resp => {
-        artiklar.splice(firstcheck ? 0 : paginate,firstcheck ? artiklar.length : 0 ,...resp)
-        localStorage.setItem("artiklar", JSON.stringify(artiklar))
-        firstcheck = false
+function laddaArtiklar(typ: Laddtyp) {
 
-    })
+    //Snabbladdar från localStorage vid ny sidladdning. Alt inkrementell laddning.
+    if (typ !== Laddtyp.incremental) {
+        //Inte inkrementell. Prövning görs om vi ska ladda från server...
+        
+        //...men först så laddar vi det som finns på local storage bara för att få snabbt innehåll på sidan.
+        artiklar = JSON.parse(localStorage.getItem("artiklar"))
+        Mithril.redraw()
+
+        //Kolla mot servern om det finns uppdaterade annars ladda från local storage
+        Mithril.request({
+            url: "https://www.sydnarkenytt.se/json/last",
+            background : true //ritar inte om sidan efter kollen.
+
+        }).then((resp) => {
+            console.log(resp)
+            let senastuppdat = resp[0].edit_date
+
+            if (Number(localStorage.getItem("lastupdate")) < senastuppdat) {
+                console.log("Dags att uppdatera")
+
+                //Det finns nya låtoss ladda dem!
+                let url = "https://www.sydnarkenytt.se/json/etta/P1"
+                m.request({
+                    url: url,
+                }).then(resp => {
+                    //Splicar artikelarrayen från grunden
+                    artiklar.splice(0, artiklar.length, ...resp)
+                    localStorage.setItem("artiklar", JSON.stringify(artiklar))
+                    localStorage.setItem("lastupdate", senastuppdat)
+                })
+            }
+        })
+    } else {
+        // Vid inkrementell alddning. Fylls arrayen bara på
+        let url = "https://www.sydnarkenytt.se/json/etta/P" + artiklar.length
+        m.request({
+            url: url
+        }).then(resp => {
+            artiklar.splice(artiklar.length, 0, ...resp)
+            localStorage.setItem("artiklar", JSON.stringify(artiklar))
+        })
+    }
 }
 
 
 //Två varianter med och utan JSX
 // JSX med två komponenter En för varje artikelpuff som tar in data via attrs. En som renderar hela listan.  
-// Föredrar ren js och inte jsx då det blir flera abstraktionsnivåer. Renare kod.
 
 let Artikel = {
-    view: (vnode)=>{
+    view: (vnode) => {
         return (
-        <div>
-            {vnode.attrs.src ? (<img width="300px" src={"https:" + vnode.attrs.src}/>) : (<img alt="Ingen bild"/>)}
-            <h1>{(vnode.attrs.title.replace(/&quot;/g, '"'))}</h1>
-            <p>{vnode.attrs.ingress}</p>
-        </div>)
+            <div>
+                {vnode.attrs.src ? (<img style="height:169px" height="169px" width="300px" src={"https:" + vnode.attrs.src} />) : (<img src="fel.png" alt="Ingen bild" />)}
+                <h1>{(vnode.attrs.title.replace(/&quot;/g, '"'))}</h1>
+                <p>{vnode.attrs.ingress}</p>
+            </div>)
     }
 }
 
 let Artiklar = {
     onupdate: () => {
+        //Innehållet renderat. Flagga att det är ok att ladda vid scroll
         loadblock = false
     },
     view: () => {
         return (
             <div>
-                {artiklar.map(artikel=>{
+                <m.route.Link href="/nyheter">Nyheter</m.route.Link> <m.route.Link href="/artiklar">Artiklar</m.route.Link>
+                {artiklar.map(artikel => {
                     return (
-                        <Artikel title={artikel.title} key={artikel.id} ingress={artikel.ingress} src={artikel.ettabild}/>
+                        <Artikel title={artikel.title} key={artikel.id} ingress={artikel.ingress} src={artikel.ettabild} />
                     )
                 })}
             </div>
@@ -60,20 +95,43 @@ let Artiklar = {
     }
 }
 
-// Motsvarande i  js förutom att den inte är uppdelad i två komponenter
-//En Mithril komponent. Kan också använda JSX. Kan skapa utifrån klasser vid större projekt.
+// Motsvarande i ren js förutom att den inte är uppdelad i två komponenter
 // Data direkt från globala artiklar
 let Sidan = {
     view: () => {
         return m("div", [artiklar.map(artikel => {
-            return m("div", {key: artikel.id}, [
-                artikel.ettabild ? m("img", {style: {width: "300px"}, src: "https:" + artikel.ettabild}) : m("") ,
+            return m("div", { key: artikel.id }, [
+                artikel.ettabild ? m("img", { style: { width: "300px" }, src: "https:" + artikel.ettabild }) : m(""),
                 m("h1", artikel.title),
                 m("p", artikel.ingress)
             ])
         })])
     }
 }
+
+
+//Testar en kompinent till och routing
+let Nyheter = {
+    view: () => {
+        return (
+            <div>
+                <m.route.Link href="/nyheter">Nyheter</m.route.Link> <m.route.Link href="/artiklar">Artiklar</m.route.Link>
+
+                <h1>Testar hur routingen funkar</h1>
+            </div>
+        )
+    }
+}
+
+
+//Testar route
+Mithril.route.prefix = ""
+Mithril.route(document.body, "/artiklar", {
+    "/nyheter": Nyheter,
+    "/artiklar": Artiklar
+})
+
+
 
 //Ladda nytt när man scrollat en bit ner.
 //För att minimera att flera sidor laddas in innan nya har hämtats så switchar vi på "loadblock". Blocket tas bort efter uppdaterad view...
@@ -82,10 +140,16 @@ window.onscroll = () => {
         let { top, height } = document.body.getBoundingClientRect()
         if (top + height <= 8000) {
             loadblock = true
-            laddaArtiklar()
+            laddaArtiklar(Laddtyp.incremental)
+            console.log("scroll")
         }
     }
 }
 
-laddaArtiklar()
-Mithril.mount(document.body, Artiklar)
+//Inledande laddning av artiklar
+laddaArtiklar(Laddtyp.reload)
+
+
+
+
+
