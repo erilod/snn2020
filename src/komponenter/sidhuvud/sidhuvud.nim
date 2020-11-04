d0c57f6119892b10d9c0b9075ab52ce5028586a3
Erik <erik@lodetti.se>
2020-11-04 14:22:17 +0100

    minor

--- src/komponenter/sidhuvud/sidhuvud.nim
+++ src/komponenter/sidhuvud/sidhuvud.nim
@@ -1,9 +1,6 @@
 include karax / prelude
 
 proc sidhuvud* (inp: int): VNode =
-    proc mint (): string = 
-        return if inp > 100: "Mint är väl gott" else: "Mint är inte gott"
-
     return buildHtml(tdiv):
         tdiv(id = "sidhuvud"):
             h1: text $inp
