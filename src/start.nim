9bfed82ac70e96609b7561b0ffdf46dcda430003
Erik Lodetti <42836420+erilod@users.noreply.github.com>
2020-11-05 09:52:31 +0100

    Update start.nim
    tar bort lite test

--- src/start.nim
+++ src/start.nim
@@ -5,25 +5,12 @@ import komponenter/ [sidhuvud/sidhuvud, artikelspalt/artikelspalt]
 import karax / vstyles
 
 
-# Push och get  url
-var counter*: int
-proc scroll (): VNode =
-    result = buildHtml(h3):
-        text "scroll: " & $counter
-
-var res: cstring
-
 proc root (data: RouterData): VNode =
     result = buildHtml(tdiv):
         sidhuvud(counter)
         tdiv(class = "section mainarea"):
             tdiv(class = "s-100 m-100 l-100 container"):
                 artikelspalt()
-
-window.addEventListener("scroll") do (ev:Event):
-    counter += 1
-    echo counter
-    redraw()
 
 setRenderer root
 
